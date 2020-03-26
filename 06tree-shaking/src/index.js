b588fdd2333c53a8d7d8e97dc799ecccc65474c7
zoro <aoping@yougola.com>
2020-03-27 00:14:50 +0800

    06tree-shaking

---
+++ 06tree-shaking/src/index.js
@@ -0,0 +1,11 @@
+import { cube } from "./math.js";
+
+function component() {
+  var element = document.createElement("pre");
+  element.innerHTML = ["Hello webpack!", "5 cubed is equal to " + cube(5)].join(
+    "\n\n"
+  );
+  return element;
+}
+
+document.body.appendChild(component());
