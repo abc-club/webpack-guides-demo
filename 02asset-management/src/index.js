ac331f266a049f60bf39776027e9ba42a0bc2508
zoro <aoping@yougola.com>
2020-03-26 16:56:47 +0800

    02asset-management

---
+++ 02asset-management/src/index.js
@@ -0,0 +1,19 @@
+import _ from "lodash";
+import "./style.css";
+import Icon from "./icon.png";
+import Data from "./data.xml";
+
+function component() {
+  let element = document.createElement("div");
+
+  element.innerHTML = _.join(["Hello", "webpack"], " ");
+  element.classList.add("hello");
+  // 将图像添加到我们已经存在的 div 中。
+  var myIcon = new Image();
+  myIcon.src = Icon;
+  element.appendChild(myIcon);
+  console.log(Data);
+  return element;
+}
+
+document.body.appendChild(component());
