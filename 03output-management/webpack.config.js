60b3bd1700553648bf03a0f973c935c8847619d7
zoro <aoping@yougola.com>
2020-03-26 17:11:20 +0800

    03output-management

--- 03output-management/webpack.config.js
+++ 03output-management/webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
 
 module.exports = {
   entry: {
@@ -11,6 +12,8 @@ module.exports = {
     path: path.resolve(__dirname, "dist")
   },
   plugins: [
+    // 注意这里CleanWebpackPlugin暴露的方式改变了
+    new CleanWebpackPlugin.CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       title: "管理输出"
     })
