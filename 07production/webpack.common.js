62e4fd35573b5244c967caf83e3c0d476035a2fb
zoro <aoping@yougola.com>
2020-03-27 09:14:45 +0800

    07production

---
+++ 07production/webpack.common.js
@@ -0,0 +1,18 @@
+const path = require("path");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+module.exports = {
+  entry: {
+    app: "./src/index.js"
+  },
+  plugins: [
+    new CleanWebpackPlugin.CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: "Production"
+    })
+  ],
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist")
+  }
+};
